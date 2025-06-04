0deee181aec1d4028057be211b8c40991f7ece8a
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-06-04 13:10:06 -0500

    docs: examples added

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,8 @@ variable "region" {
   description = "AWS region to deploy to"
   type        = string
 }
+
+output "region" {
+  description = "The AWS region where the resources are deployed"
+  value       = var.region
+}
